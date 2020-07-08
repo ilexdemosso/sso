d668a470b8e231b7394a59e9843f3a7299daa0f4
Artyom Tokachev <a.tokachev@itransition.com>
2020-07-08 21:58:30 +0300

    Style fix

--- sso/forms.py
+++ sso/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
-class InputForm(forms.Form): 
-    login = forms.CharField(max_length = 200) 
-    password = forms.CharField(widget = forms.PasswordInput()) 
+
+class InputForm(forms.Form):
+    login = forms.CharField(max_length=200)
+    password = forms.CharField(widget=forms.PasswordInput())
--- sso/views.py
+++ sso/views.py
@@ -2,15 +2,18 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import InputForm
-import requests, base64
+import requests
+import base64
 import json
 import jwt
 import os
 
-def login(request): 
-    context ={} 
-    context['form']= InputForm()
+
+def login(request):
+    context = {}
+    context['form'] = InputForm()
     return render(request, "home.html", context)
+
 
 def authorize(request):
     if request.method == 'POST':
@@ -19,21 +22,34 @@ def authorize(request):
         if form.is_valid():
             login = form.data['login']
             password = form.data['password']
-            
-            b64credentials = base64.b64encode(f'{login}:{password}'.encode()).decode()
-            response1 = requests.post('https://consplusweb-dev:8097/backend/oauth/code/user?client_id=test', headers={'Authorization': f'Basic {b64credentials}'}, verify=False).json()
+
+            b64credentials = base64.b64encode(
+                f'{login}:{password}'.encode()).decode()
+            response1 = requests.post(
+                'https://consplusweb-dev:8097/backend/oauth/code/user?client_id=test',
+                headers={
+                    'Authorization': f'Basic {b64credentials}'},
+                verify=False).json()
             if 'code' in response1:
                 code = response1['code']
 
                 b64secret = base64.b64encode(f'test:secret'.encode()).decode()
-                
-                response2 = requests.post(f'https://consplusweb-dev:8097/backend/oauth/code/client?code={code}', headers={'Authorization': f'Basic {b64secret}'}, verify=False).json()
+
+                response2 = requests.post(
+                    f'https://consplusweb-dev:8097/backend/oauth/code/client?code={code}',
+                    headers={
+                        'Authorization': f'Basic {b64secret}'},
+                    verify=False).json()
                 if 'access_token' in response2:
                     access_token = response2['access_token']
-                    public_key_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'jwtRS256.key.pub')
+                    public_key_path = os.path.join(
+                        os.path.dirname(
+                            os.path.realpath(__file__)),
+                        'jwtRS256.key.pub')
                     public_key = open(public_key_path).read()
-                    
-                    payload = jwt.decode(access_token, public_key, algorithms=['RS256'])
+
+                    payload = jwt.decode(
+                        access_token, public_key, algorithms=['RS256'])
                     first_name = payload['first_name']
                     last_name = payload['last_name']
                     return HttpResponse(f'You are {first_name} {last_name}')
